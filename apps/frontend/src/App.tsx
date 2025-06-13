c77aceafb48c38ca334bfd9cbb60858e638c1acc
Twin Edo <twinedo.dev@gmail.com>
2025-06-13 08:10:31 +0700

    change shared name from @repo-shared to ~repo-shared

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import type { ProfileProps } from '@repo-shared';
+import type { ProfileProps } from '~repo-shared';
 
 
 type ModuleWithProfile = {
